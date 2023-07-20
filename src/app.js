318f6ea7ed2071320e490ef09e71e7590f4ab703
nixena <nikanozadze1010@gmail.com>
2023-07-20 20:05:36 +0400

    fix: try to fix code async

--- src/app.js
+++ src/app.js
@@ -4,25 +4,28 @@ import pool, { createBooksTable } from "./config/sql.js";
 const app = express();
 
 
-app.get('/books', async (req, res) => {
-    try {
-        const queryResult = await pool.query('SELECT * FROM books');
-        const books = queryResult.rows;
-        res.json(books);
-    } catch (err) {
-        console.error('Error fetching books:', err);
-        res.status(500).send('Error fetching books');
-    }
-});
-
-
 async function initialize() {
     try {
-      await createBooksTable();
-      app.listen(3000);
+      await createTable();
+      startServer();
     } catch (err) {
       console.error('Error creating table:', err);
     }
   }
+  
+  function startServer() {
+    app.get('/books', async (req, res) => {
+      try {
+        const queryResult = await pool.query('SELECT * FROM books');
+        const books = queryResult.rows;
+        res.json(books);
+      } catch (err) {
+        console.error('Error fetching books:', err);
+        res.status(500).send('Error fetching books');
+      }
+    });
+  
+    app.listen(3000);
+  }
 
-initialize();
+  initialize();
