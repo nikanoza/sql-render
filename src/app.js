3696cef26e8b68650c81a28c82b6ed4dea396fec
nixena <nikanozadze1010@gmail.com>
2023-07-20 20:01:00 +0400

    feat: add books get request

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 import express from "express";
-import { createBooksTable } from "./config/sql.js";
+import pool, { createBooksTable } from "./config/sql.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.status(200).json({message: "works!"});
+
+app.get('/books', async (req, res) => {
+    try {
+        const queryResult = await pool.query('SELECT * FROM books');
+        const books = queryResult.rows;
+        res.json(books);
+    } catch (err) {
+        console.error('Error fetching books:', err);
+        res.status(500).send('Error fetching books');
+    }
 });
+
 
 async function initialize() {
     try {
       await createBooksTable();
-      console.log('Table "books" created successfully!');
-      app.listen(3000, () => {
-        console.log(`Server is running!`);
-      });
+      app.listen(3000);
     } catch (err) {
       console.error('Error creating table:', err);
     }
